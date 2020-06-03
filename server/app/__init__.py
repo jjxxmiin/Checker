5a1f035d4838e1b93f2576cbdf6e253efb7591eb
jjeamin <woalsdl500@naver.com>
2020-06-04 02:11:06 +0900

    api.py

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,13 +1,15 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))  # database 경로를 절대경로로 설정함
-dbfile = os.path.join(os.path.join(basedir, 'db'), 'db.sqlite') # 데이터베이스 이름과 경로
+DB_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)) , 'db')
+dbfile = os.path.join(DB_PATH, 'db.sqlite') # 데이터베이스 이름과 경로
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
 app.config['SECRET_KEY'] = '8427'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+from app import models
